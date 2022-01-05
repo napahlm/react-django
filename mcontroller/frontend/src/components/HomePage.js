a9755c0029f29b8ea03d44eb46ef5e940d7203ed
pahlm <borgepahlm@gmail.com>
2022-01-05 18:14:55 +0100

    Clean up code

--- mcontroller/frontend/src/components/HomePage.js
+++ mcontroller/frontend/src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
-import Room from './Room';
+import Room from "./Room";
 import {
     BrowserRouter as Router,
     Routes,
@@ -17,14 +17,21 @@ export default class HomePage extends Component {
     }
 
     render() {
-        return (<Router>
+        return (
+        <Router>
             <Routes>
                 <Route exact path="/" element={
                     <p>This is the home page.</p>
                 }></Route>
-                <Route path="/join" element={<RoomJoinPage/>}></Route>
-                <Route path="/create" element={<CreateRoomPage/>}></Route>
-                <Route path='/room/:roomCode' component={Room} />
+                <Route path="/join" >
+                    <RoomJoinPage />
+                </Route>
+                <Route path="/create" >
+                    <CreateRoomPage />
+                    </Route>
+                <Route path="/room/:roomCode" >
+                    <Room />
+                </Route>
             </Routes>
         </Router>
         );
--- mcontroller/frontend/src/components/Room.js
+++ mcontroller/frontend/src/components/Room.js
@@ -25,13 +25,12 @@ export default class Room extends Component {
     }
 
     render() {
-        return (
+        return
             <div>
                 <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
                 <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
                 <p>Host: {this.state.isHost.toString()}</p>
             </div>
-        );
     }
 }
