56482a1ce78b8ee187f725264d4b11d4a2e7c270
pahlm <borgepahlm@gmail.com>
2022-01-05 20:45:38 +0100

    Update Room.js

--- mcontroller/frontend/src/components/Room.js
+++ mcontroller/frontend/src/components/Room.js
@@ -13,24 +13,25 @@ export default class Room extends Component {
     }
 
     getRoomDetails() {
-        fetch('/api/get-room' + '?code=' + this.roomCode).then((response) => 
-            response.json()
-        ).then((data) => {
-            this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pause,
-                isHost: data.is_host,
+        fetch("/api/get-room" + "?code=" + this.roomCode)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pause,
+                    isHost: data.is_host,
             });
         });
     }
 
     render() {
-        return
+        return (
             <div>
                 <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
                 <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
                 <p>Host: {this.state.isHost.toString()}</p>
             </div>
+        );
     }
 }
