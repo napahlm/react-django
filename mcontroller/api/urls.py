d353320180b57b48dff55e12fbd074a85535765c
pahlm <borgepahlm@gmail.com>
2022-01-06 10:33:55 +0100

    Update urls.py

--- mcontroller/api/urls.py
+++ mcontroller/api/urls.py
@@ -4,5 +4,5 @@ from .views import CreateRoomView, RoomView, GetRoom
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
 ]
